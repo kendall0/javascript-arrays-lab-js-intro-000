60cd88ec9d572d13fa0b08c2d5ba5a4efbd9f7cc
Kendall George <kendallanngeorge@gmail.com>
2021-02-26 21:02:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield']; //define your array here
 
 // Add your functions and code here
+
+//appends a kitten to the end of the kittens array
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name) {
+  [...kittens, name];
+  return kittens;
+}
