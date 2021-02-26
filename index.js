5a977579a364f19f22379f9256c24d689c370f3a
Kendall George <kendallanngeorge@gmail.com>
2021-02-26 21:08:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,3 +34,8 @@ function prependKitten(name) {
 function removeLastKitten() {
   kittens.slice(0, kittens.length - 1);
 }
+
+function removeFirstKitten() {
+  kittens.slice(1);
+  return kittens
+}
